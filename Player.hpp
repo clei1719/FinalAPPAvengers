ac054e378b5c84fbdbf4e8f6c7282577772ff11e
Clint Eisenzimmer <49416719+clei1719@users.noreply.github.com>
2019-04-16 18:49:20 -0600

    Player.hpp
    Player Class File

---
+++ Player.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+
+#include "Vertex.hpp"
+// Implimentation of just the Vertex(s)
+#ifndef Player_HPP
+#define Player_HPP
+
+struct Player
+{
+        Vertex *playerLocation;
+};
+#endif
